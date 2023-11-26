f830c424c3f02e2a64b872fdd956ae3704403928
Sezoka <vlad.novport@gmail.com>
2023-11-26 23:34:31 +0300

    upd

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include <raylib.h>
-
-#define DEFAULT_SCR_W 800
-#define DEFAULT_SCR_H 600
+#include <stdlib.h>
 
 #define TETRIS_FIELD_W 10
-#define TETRIS_FIELD_H 40
+#define TETRIS_FIELD_H 20
+
+#define DEFAULT_SCR_W (TETRIS_FIELD_W * 48)
+#define DEFAULT_SCR_H (TETRIS_FIELD_H * 48)
 
 #define TETRIS_FIELD_LEN 400
 
-#define SHAPE_W 4
-#define SHAPE_H 4
-#define SHAPE_LEN 16
+#define SHAPE_FIELD_W 4
+#define SHAPE_FIELD_H 4
+#define SHAPE_FIELD_LEN 16
+
+#define SHAPE_FALL_DELAY 0.3
 
 typedef enum {
   Clr_Black,
@@ -26,6 +30,7 @@ typedef enum {
 } Clr;
 
 typedef enum {
+  Shape_None,
   Shape_I,
   Shape_J,
   Shape_L,
@@ -33,10 +38,34 @@ typedef enum {
   Shape_S,
   Shape_T,
   Shape_Z,
-} Shape;
+  Shape_Size,
+} ShapeKind;
 
-Clr get_clr_of_shape(Shape sh) {
-  switch (sh) {
+typedef struct {
+  Clr field[TETRIS_FIELD_LEN];
+  Clr shape[SHAPE_FIELD_LEN];
+  int shape_x;
+  int shape_y;
+  int shape_w;
+  float fall_delay;
+  float curr_fall_delay;
+} State;
+
+typedef enum {
+  ShCol_None,
+  ShCol_Block,
+  ShCol_Border,
+} ShapeCollision;
+
+typedef struct {
+  int w;
+  int h;
+} Size;
+
+Clr get_clr_of_shape(ShapeKind kind) {
+  switch (kind) {
+  case Shape_None:
+    return Clr_Black;
   case Shape_I:
     return Clr_LightBlue;
   case Shape_J:
@@ -51,6 +80,8 @@ Clr get_clr_of_shape(Shape sh) {
     return Clr_Violet;
   case Shape_Z:
     return Clr_Red;
+  case Shape_Size:
+    return Clr_Black;
   }
 }
 
@@ -75,14 +106,6 @@ Color clr_to_raylib_color(Clr clr) {
   }
 }
 
-typedef struct {
-  Clr field[TETRIS_FIELD_LEN];
-  Clr shape[SHAPE_LEN];
-  int shape_x;
-  int shape_y;
-
-} State;
-
 void draw_field(State *s) {
   int block_size = DEFAULT_SCR_W / TETRIS_FIELD_W;
   for (int y = 0; y < TETRIS_FIELD_H; y += 1) {
@@ -91,32 +114,223 @@ void draw_field(State *s) {
       DrawRectangle(x * block_size, y * block_size, block_size, block_size,
                     color);
       DrawRectangleLines(x * block_size, y * block_size, block_size, block_size,
-                         GRAY);
+                         DARKBROWN);
     }
   }
 
-  for (int y = 0; y < SHAPE_H; y += 1) {
-    for (int x = 0; x < SHAPE_W; x += 1) {
-      Color color = clr_to_raylib_color(s->field[y * SHAPE_W + x]);
-      DrawRectangle(x * block_size, y * block_size, block_size, block_size,
-                    color);
-      DrawRectangleLines(x * block_size, y * block_size, block_size, block_size,
-                         GRAY);
+  for (int y = 0; y < SHAPE_FIELD_H; y += 1) {
+    for (int x = 0; x < SHAPE_FIELD_W; x += 1) {
+      Clr clr = s->shape[y * SHAPE_FIELD_W + x];
+      if (clr != Clr_Black) {
+        Color color = clr_to_raylib_color(clr);
+        DrawRectangle(((s->shape_x + x) * block_size),
+                      ((s->shape_y + y) * block_size), block_size, block_size,
+                      color);
+      }
     }
   }
+}
+
+void clear_shape(State *s) {
+  for (int i = 0; i < SHAPE_FIELD_LEN; i += 1) {
+    s->shape[i] = Clr_Black;
+  }
+}
+
+int get_shape_width(ShapeKind kind) {
+  switch (kind) {
+  case Shape_I:
+    return 4;
+  case Shape_O:
+    return 2;
+  case Shape_L:
+  case Shape_J:
+  case Shape_S:
+  case Shape_T:
+  case Shape_Z:
+    return 3;
+  case Shape_Size:
+  case Shape_None:
+    return 0;
+  }
+}
+
+void spawn_random_shape(State *s) {
+  ShapeKind kind = (ShapeKind)GetRandomValue(1, Shape_Size - 1);
+
+  Clr clr = get_clr_of_shape(kind);
+
+  clear_shape(s);
+
+  switch (kind) {
+  case Shape_I:
+    s->shape[SHAPE_FIELD_W * 1 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 2] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 3] = clr;
+    break;
+  case Shape_J:
+    s->shape[SHAPE_FIELD_W * 1 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 2] = clr;
+    break;
+  case Shape_L:
+    s->shape[SHAPE_FIELD_W * 2 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 2] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 2] = clr;
+    break;
+  case Shape_O:
+    s->shape[SHAPE_FIELD_W * 0 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 0 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 1] = clr;
+    break;
+  case Shape_S:
+    s->shape[SHAPE_FIELD_W * 0 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 0 + 2] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 1] = clr;
+    break;
+  case Shape_T:
+    s->shape[SHAPE_FIELD_W * 1 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 2 + 2] = clr;
+    break;
+  case Shape_Z:
+    s->shape[SHAPE_FIELD_W * 0 + 0] = clr;
+    s->shape[SHAPE_FIELD_W * 0 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 1] = clr;
+    s->shape[SHAPE_FIELD_W * 1 + 2] = clr;
+    break;
+  case Shape_Size:
+  case Shape_None:
+    printf("spawn_random_shape switch\n");
+  }
+
+  int shape_w = get_shape_width(kind);
+  s->shape_x = shape_w / 2;
+  s->shape_y = 0;
+  s->shape_w = shape_w;
 }
 
 void init_state(State *s) {
   for (int i = 0; i < TETRIS_FIELD_LEN; i += 1)
     s->field[i] = Clr_Black;
-  for (int i = 0; i < SHAPE_LEN; i += 1)
-    s->shape[i] = Clr_Black;
+  clear_shape(s);
   s->shape_x = 0;
   s->shape_y = 0;
+  s->shape_w = 0;
+  s->fall_delay = SHAPE_FALL_DELAY;
+  s->curr_fall_delay = 0.0;
+}
+
+ShapeCollision check_shape_collision(State *s) {
+  for (int y = 0; y < SHAPE_FIELD_H; y += 1) {
+    for (int x = 0; x < SHAPE_FIELD_W; x += 1) {
+      if (s->shape[y * SHAPE_FIELD_W + x] != Clr_Black) {
+        int field_x = x + s->shape_x;
+        int field_y = y + s->shape_y;
+
+        if ((field_x < 0 || TETRIS_FIELD_W <= field_x) ||
+            (field_y < 0 || TETRIS_FIELD_H <= field_y)) {
+          return ShCol_Border;
+        }
+
+        if (s->field[field_y * TETRIS_FIELD_W + field_x] != Clr_Black) {
+          return ShCol_Block;
+        }
+      }
+    }
+  }
+
+  return ShCol_None;
+}
+
+// 1 2 3 4
+// 5 6 7 8
+// 9 A B C
+// D E F J
+
+void rotate_shape(State *s) {
+  Clr tmp_shape[SHAPE_FIELD_LEN];
+
+  for (int y = 0; y < s->shape_w; y += 1) {
+    for (int x = 0; x < s->shape_w; x += 1) {
+      Clr shape_clr = s->shape[y * SHAPE_FIELD_W + x];
+
+      int x1 = SHAPE_FIELD_W - 1 - y;
+      int y1 = x;
+
+      tmp_shape[y1 * SHAPE_FIELD_W + x1] = shape_clr;
+    }
+  }
+
+  for (int y = 0; y < s->shape_w; y += 1) {
+    for (int x = 0; x < s->shape_w; x += 1) {
+      int offset = y * SHAPE_FIELD_W + x;
+      Clr shape_clr = tmp_shape[offset];
+      s->shape[offset] = shape_clr;
+    }
+  }
+}
+
+void handle_input(State *s) {
+  int prev_x = s->shape_x;
+  int prev_y = s->shape_y;
+
+  if (IsKeyDown(KEY_A)) {
+    s->shape_x -= 1;
+  }
+  if (IsKeyDown(KEY_D)) {
+    s->shape_x += 1;
+  }
+  if (IsKeyDown(KEY_S)) {
+    s->shape_y += 1;
+  }
+  if (IsKeyDown(KEY_W)) {
+    rotate_shape(s);
+  }
+
+  if (check_shape_collision(s) != ShCol_None) {
+    s->shape_x = prev_x;
+    s->shape_y = prev_y;
+  }
+}
+
+void bake_shape(State *s) {
+  for (int y = 0; y < SHAPE_FIELD_H; y += 1) {
+    for (int x = 0; x < SHAPE_FIELD_W; x += 1) {
+      Clr shape_clr = s->shape[y * SHAPE_FIELD_W + x];
+      if (shape_clr != Clr_Black) {
+        int field_x = x + s->shape_x;
+        int field_y = y + s->shape_y;
+        s->field[field_y * TETRIS_FIELD_W + field_x] = shape_clr;
+      }
+    }
+  }
+}
+
+void update(State *s) {
+  float delta = GetFrameTime();
+
+  s->curr_fall_delay += delta;
+
+  if (s->fall_delay <= s->curr_fall_delay) {
+    s->curr_fall_delay = 0;
+    s->shape_y += 1;
+    if (check_shape_collision(s) != ShCol_None) {
+      s->shape_y -= 1;
+      bake_shape(s);
+      spawn_random_shape(s);
+    }
+  }
 }
 
 int main() {
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  // SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
   InitWindow(DEFAULT_SCR_W, DEFAULT_SCR_H, "Tetris");
   SetTargetFPS(60);
@@ -124,10 +338,15 @@ int main() {
   State state;
   init_state(&state);
 
+  spawn_random_shape(&state);
+
   while (!WindowShouldClose()) {
+    handle_input(&state);
+    update(&state);
+
     BeginDrawing();
+    ClearBackground(BLACK);
     draw_field(&state);
-    ClearBackground(RAYWHITE);
     EndDrawing();
   }
 
